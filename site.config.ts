29885e23215130a2a990f3a1dd4705cedd8dea99
羲奕 <100599604+Taya163@users.noreply.github.com>
2022-07-07 11:16:28 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '8eb85da06eca452f90232291aa136953',
+  rootNotionPageId: 'TayaW-s-BLOG-8919dcfe543342f69ecd1a5bd656ce08',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -66,7 +66,7 @@ export default siteConfig({
     },
     {
       title: '首页',
-      pageId: '8eb85da06eca452f90232291aa136953'
+      pageId: 'TayaW-s-BLOG-8919dcfe543342f69ecd1a5bd656ce08'
     }
   ]
 })
